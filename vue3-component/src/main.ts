9a20924016957af811a1039f306d33bfdc17f888
anglefly <1406909912@qq.com>
2020-08-27 16:02:01 +0800

    增加 elemenUI，增加 utils 工具包 自定义form配置表单

--- vue3-component/src/main.ts
+++ vue3-component/src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from "vue";
-import App from "./App";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router';
+import store from './store';
 
 createApp(App)
   .use(store)
   .use(router)
-  .mount("#app");
+  .use(ElementUI)
+  .mount('#app');
